fb15fc8ed2b47b1f2e60b99f6ede4ba473a8a012
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2019-11-03 13:22:02 +0000

    (chore) death to comments

--- main.tf
+++ main.tf
@@ -126,7 +126,6 @@ resource "azurerm_network_interface" "default" {
 
 resource "random_id" "randomId" {
   keepers = {
-    # Generate a new ID only when a new resource group is defined
     resource_group = azurerm_resource_group.default.name
   }
 
